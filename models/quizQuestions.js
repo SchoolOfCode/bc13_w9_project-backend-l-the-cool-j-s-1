ad0d2a68fc6286d84f57fb19e2140a6bf7469a4d
lani-irving <laniirving@googlemail.com>
2022-11-29 16:16:40 +0000

    quizquestions tests

--- models/quizQuestions.js
+++ models/quizQuestions.js
@@ -4,10 +4,9 @@ export async function getQuizQuestions() {
   // Query the database and return all questions
   // Use the query function from db/index.js
   // Call the query function and pass in the SQL query
-  const result = await query("SELECT * FROM quizquestions");
+  const result = await pool.query("SELECT * FROM quizquestions");
   // Return the result of the query
   // Need to use await to wait for the query to finish
   // await query("SQL query here");
   return result.rows;
 }
-
--- routes/quizquestions.test.js
+++ routes/quizquestions.test.js
@@ -2,24 +2,23 @@ import app from "../app.js";
 import request from "supertest";
 
 describe("Get /quizquestions", function () {
-  it("Get all interview", async function () {
+  it("Get all quizquestions", async function () {
     const response = await request(app).get("/quizquestions");
     console.log(response);
   });
-//   it("Get status code", async function () {
-//     const response = await request(app).get("/quizquestions");
-//     expect(response.statusCode).toBe(200);
-//   });
-//   it("Get body`s has payload", async function () {
-//     const response = await request(app).get("/quizquestions");
-//     expect(response.body).toHaveProperty("payload");
-//   });
-//   it("Get body`s structure", async function () {
-//     const response = await request(app).get("/quizquestions");
-//     expect(response.body).toStrictEqual({
-//       success: true,
-//       payload: expect.any(Array),
-//     });
-//   });
-    
+  it("Get status code", async function () {
+    const response = await request(app).get("/quizquestions");
+    expect(response.statusCode).toBe(200);
+  });
+  it("Get body`s has payload", async function () {
+    const response = await request(app).get("/quizquestions");
+    expect(response.body).toHaveProperty("payload");
+  });
+  it("Get body`s structure", async function () {
+    const response = await request(app).get("/quizquestions");
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: expect.any(Array),
+    });
+  });
 });
